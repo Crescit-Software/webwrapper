976c305abdba7498601ebb06e63ca82de78f808b
Philip Borbon <philip@crescit.com>
2024-03-11 10:30:10 +0800

    Initial commit

---
+++ Source/WebViewController+WebView.swift
@@ -0,0 +1,49 @@
+import WebKit
+
+extension WebViewController {
+    /**
+     Sets up the main web view.
+     */
+    @objc open func setupWebView() {
+        let webViewConfig = WebViewConfiguration()
+        
+        webView = WKWebView(frame: view.bounds, configuration: webViewConfig)
+        webView.allowsBackForwardNavigationGestures = true
+        setupDelegates()
+        
+        view.insertSubview(webView, at: 0)
+        autolayoutWebView(webView)
+    }
+    
+    open func setupDelegates() {
+        if let url = rootURL {
+            if navigationDelegate == nil {
+                navigationDelegate = WebViewNavigationDelegate(rootURL: url, delegate: self)
+            }
+            webView.navigationDelegate = navigationDelegate
+            uiDelegate = WebViewUIDelegate(baseURL: url)
+        }
+        
+        uiDelegate?.delegate = self
+        webView.uiDelegate = uiDelegate
+    }
+    
+    /**
+     Autolayout the main web view.
+     
+     - parameter webView: The web view to layout.
+     */
+    open func autolayoutWebView(_ webView: WKWebView) {
+        guard let superview = webView.superview else {
+            fatalError()
+        }
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        let views = ["webView":webView]
+        superview.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[webView(>=0)]|",
+            options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: views))
+        
+        superview.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[webView(>=0)]|",
+            options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: views))
+    }
+ 
+}
